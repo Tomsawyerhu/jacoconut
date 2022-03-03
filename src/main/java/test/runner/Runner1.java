5313a56606e305ffa6b7198ade265824dfc053ff
Tomsawyerhu <1095549886@qq.com>
2022-03-04 01:23:45 +0800

    clean

--- src/main/java/test/runner/Runner1.java
+++ src/main/java/test/runner/Runner1.java
@@ -1,7 +1,6 @@
 package test.runner;
 
 import api.JacoconutApi;
-import api.LCType;
 
 import java.io.IOException;
 
@@ -9,7 +8,7 @@ public class Runner1 {
     public static void main(String[] args) {
         String classFile="D:\\BaiduNetdiskDownload\\maven-projects\\maven-projects\\commons-cli-cli-1.4\\target\\classes\\org\\apache\\commons\\cli\\Parser.class";
         try {
-            JacoconutApi.lineCoverageProbe(classFile, LCType.BASIC_BLOCK);
+            JacoconutApi.lineCoverageProbe(classFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
